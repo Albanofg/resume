8ebd708c07c69070821a4f9e3f404e9dd1b41dce
albano <gonzalezalbanof@gmail.com>
2023-07-04 03:18:51 -0300

    gallery

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -10,38 +10,46 @@ const Gallery = () => {
   return (
     <div className="gallery">
          
-      <div className="cards">
-        <div className="info">
-          <i className="icon"><FaReact/></i>
-          <p className="txtInfo">Código en GitHub de mi proyecto final para el curso de ReactJs de CoderHouse</p>
+      <a href="https://github.com/Albanofg/tienda-fotos" target="_blank">
+        <div className="cards">
+          <div className="info">
+            <i className="icon"><FaReact/></i>
+            <p className="txtInfo">Código en GitHub de mi proyecto final <br /> para el curso de ReactJs de CoderHouse</p>
+          </div>
+          <img className="imgCards" src="https://www.mediafire.com/convkey/82d6/v1e0ib9o22eyawr9g.jpg" alt="ReactJs"/>
         </div>
-        <img className="imgCards" src="https://www.mediafire.com/convkey/82d6/v1e0ib9o22eyawr9g.jpg" alt="ReactJs"/>
-      </div>
+      </a>
       
-      <div className="cards">
-        <div className="info">
-          <i className="icon"><SiJavascript/></i>
-          <p className="txtInfo">Código en GitHub de mi proyecto final para el curso de Javascript de CoderHouse</p>
+      <a href="https://github.com/Albanofg/TonicoHnos" target="_blank">
+        <div className="cards">
+          <div className="info">
+            <i className="icon"><SiJavascript/></i>
+            <p className="txtInfo">Código en GitHub de mi proyecto final <br /> para el curso de Javascript de CoderHouse</p>
+          </div>
+          <img className="imgCards" src="https://www.mediafire.com/convkey/80f8/e4cobhttvkkjbia9g.jpg" alt="Javascript"/>
         </div>
-        <img className="imgCards" src="https://www.mediafire.com/convkey/80f8/e4cobhttvkkjbia9g.jpg" alt="Javascript"/>
-      </div>
+      </a>
 
-      <div className="cards">
-        <div className="info">
-          <i className="icon"><GoBriefcase/></i>
-          <p className="txtInfo">Codigo en GitHub de ésta página</p>
+      <a href="https://github.com/Albanofg/resume" target="_blank">
+        <div className="cards">
+          <div className="info">
+            <i className="icon"><GoBriefcase/></i>
+            <p className="txtInfo">Codigo en GitHub de ésta página</p>
+          </div>
+          <img className="imgCards" src="https://www.mediafire.com/convkey/8fae/omcs62cd8v3h9mozg.jpg" alt="Portfolio"/>
         </div>
-        <img className="imgCards" src="https://www.mediafire.com/convkey/8fae/omcs62cd8v3h9mozg.jpg" alt="Portfolio"/>
-      </div>
+      </a>
 
-      <div className="cards">
-        <div className='info'>
-          <i className="icon"><BsFillShieldLockFill/></i>
-          <p className="txtInfo">Análisis final para el curso de Ciberseguridad de CoderHouse</p>
+      <a href="https://drive.google.com/file/d/1k5qSbrR5Olx3V4mEHc62gGF83HUyh44J/view?usp=sharing" target='_blank'>
+        <div className="cards">
+          <div className='info'>
+            <i className="icon"><BsFillShieldLockFill/></i>
+            <p className="txtInfo">Análisis final para el curso de <br /> Ciberseguridad de CoderHouse</p>
+          </div>
+          <img className="imgCards" src="https://www.mediafire.com/convkey/1f79/hkjvc0dbk559e3bzg.jpg" alt="CySec"/>
         </div>
-        <img className="imgCards" src="https://www.mediafire.com/convkey/1f79/hkjvc0dbk559e3bzg.jpg" alt="CySec"/>
-      </div>
-
+      </a>
+      
     </div> 
   )
 }
